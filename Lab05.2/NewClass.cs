5687df7c63bf380911a63f367ef35c13156eb67d
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-28 09:47:14 +0300

    Добавьте файлы проекта.

---
+++ Lab05.2/NewClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab05._2
+{
+    class NewClass
+    {
+        public static int NewMethod(double a, double b, double c, ref double x1, ref double x2 )
+        {
+            var discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0) // Квадратное уравнение не имеет корней.
+            {
+                x1 = x2 = 0;
+                return -1;
+            }
+            else
+            {
+                if (discriminant == 0) // Оба корня совпадают.
+                {
+                    x1 = -b / (2 * a);
+                    return 0;
+                }
+                else // Корни найдены.
+                {
+                    x1 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                    x2 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                    return 1;
+                }
+            }
+        }
+    }
+}
---
+++ Lab05.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Lab05._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = new();
+            double x2 = new();
+
+            Console.WriteLine("Вычисление корней квадратного уравнения");
+
+            Console.WriteLine("Введите первый (старший) коэффициент a: ");
+            var a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второй (средний) коэффициент b: ");
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третий (свободный) коэффициент c: ");
+            var c = double.Parse(Console.ReadLine());
+
+            switch (NewClass.NewMethod(a, b, c, ref x1, ref x2))
+            {
+                case -1: // Квадратное уравнение не имеет корней.
+                    {
+                        Console.WriteLine($"Кореней уравнения с коэффициентами a={a}, b={b}, c={c} нет.");
+                        break;
+                    }
+                case 0: // Оба корня совпадают.
+                    {
+                        Console.WriteLine($"Корень уравнения с коэффициентами a={a}, b={b}, c={c} один: x1=x2={x1}.");
+                        break;
+                    }
+                case 1: // Корни найдены.
+                    {
+                        Console.WriteLine($"Корени уравнения с коэффициентами a={a}, b={b}, c={c} равны: x1={x1}, x2={x2}.");
+                        break;
+                    }
+
+            }
+        }
+
+
+    }
+}
